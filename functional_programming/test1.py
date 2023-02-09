6d098b36c58928151c7c760f2be8c8515c6e1dde
Gcan Development <94870493+GcanDev@users.noreply.github.com>
2023-02-10 00:20:04 +0100

    Create test1.py

---
+++ functional_programming/test1.py
@@ -0,0 +1,20 @@
+nums = (2, 12, 5, 8, 9, 3, 16, 7, 13, 19, 21, 1, 15, 4, 22, 20, 11)
+
+# Checkpoint 1 code goes here.
+greater_than_10_doubled=map(lambda x: x*2, filter(lambda x: x>10, nums))
+print(tuple(greater_than_10_doubled))
+# Translate this code into functional:
+"""
+lst = []
+for i in nums:
+  if i % 3 == 0:
+    lst.append(i)
+ 
+for i in range(len(lst)):
+  lst[i] = lst[i] * 3
+ 
+tuple(lst)
+"""
+# Checkpoint 2 code goes here.
+functional_way=map(lambda x: x*3, filter(lambda x: x%3==0, nums))
+print(tuple(functional_way))
